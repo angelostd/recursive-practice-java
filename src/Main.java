cde8de3d1fa7dd63da5228ae06d4c83ef0c62bae
Angel Duarte <adavid.tinocod@gmail.com>
2022-09-25 12:16:30 -0500

    neg expo supported :D

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,13 @@ public class Main {
             reader.close();
         } catch (Exception e) {
             // TODO: handle exception
-            System.out.println("Ha ocurrido un error");
+            System.out.println("Ha ocurrido un error: " + e);
         }
     }
 
-    public static int power(int base, int exponent) {
-        return (exponent <= 0) ? 1 : base * power(base, exponent - 1);
+    public static double power(int base, int exponent) {
+        boolean positive = (exponent >= 0);
+        double result = (exponent == 0) ? 1 : base * power(base, Math.abs(exponent) - 1);
+        return positive ? result : (1 / result);
     }
 }
