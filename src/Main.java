fd5da61965f632d7f606e5256d29f932a41b64b1
Angel Duarte <adavid.tinocod@gmail.com>
2022-09-25 11:09:21 -0500

    styled and handled exceps

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@ public class Main {
     public static void main(String[] args) {
         try {
             Scanner reader = new Scanner(System.in);
-            System.out.print("Bienvenid@ a la calculadora de potencias!\n\n >>: Introduce la base\n\n <<: ");
+            System.out.print("=> Bienvenid@ a la calculadora de potencias! <=\n>>: Introduce la base\n<<: ");
             int base = reader.nextInt();
-            System.out.print(">>: Introduce el exponente\n\n<<:");
+            System.out.print("\n>>: Introduce el exponente\n<<: ");
             int exponent = reader.nextInt();
-            System.out.println("=> El resultado es : " + power(base, exponent));
+            System.out.println("\n=> El resultado es : " + power(base, exponent));
             reader.close();
         } catch (Exception e) {
             // TODO: handle exception
+            System.out.println("Ha ocurrido un error");
         }
     }
 
     public static int power(int base, int exponent) {
-        return (exponent == 0)? 1 :base * power(base, exponent-1);
+        return (exponent <= 0) ? 1 : base * power(base, exponent - 1);
     }
 }
